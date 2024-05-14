2b0b14ad7be4794d4af88184fbbc9457f716a1fa
1emu <github@lemu.sh>
2024-05-14 12:15:20 -0300

    fix: explicit BoxTabs props for TS

--- src/components/Common/BoxTabs.tsx
+++ src/components/Common/BoxTabs.tsx
@@ -1,8 +1,21 @@
+import * as React from 'react'
+
 import { Tabs } from 'decentraland-ui/dist/components/Tabs/Tabs'
 
 import './BoxTabs.css'
 
-class BoxTabs extends Tabs {
+interface BoxTabsProps {
+  isFullscreen?: boolean
+  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  className?: string
+  children?: React.ReactNode
+}
+
+class BoxTabs extends React.PureComponent<BoxTabsProps> {
+  static Left = Tabs.Left
+  static Right = Tabs.Right
+  static Tab = Tabs.Tab
+
   render() {
     return (
       <div className="BoxTabs">
