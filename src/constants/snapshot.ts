83c1b19c1588d81a09e7381baffd21535a79f094
Andy Espagnolo <andyespagnolo@gmail.com>
2024-06-12 12:37:08 -0300

    refactor: remove unused variable (#99)

--- src/constants/snapshot.ts
+++ src/constants/snapshot.ts
@@ -3,7 +3,6 @@ import { config } from '../config'
 export const SNAPSHOT_SPACE = config.get('SNAPSHOT_SPACE')
 export const SNAPSHOT_DURATION = Number(config.get('SNAPSHOT_DURATION'))
 export const SNAPSHOT_URL = config.get('SNAPSHOT_URL')
-export const SNAPSHOT_QUERY_ENDPOINT = config.get('SNAPSHOT_QUERY_ENDPOINT')
 export const SNAPSHOT_API = config.get('SNAPSHOT_API')
 export const SNAPSHOT_DELEGATION_URL = `https://snapshot.org/#/delegate/${SNAPSHOT_SPACE}`
 export const SNAPSHOT_DELEGATE_CONTRACT_ADDRESS = config.get('SNAPSHOT_DELEGATE_CONTRACT_ADDRESS')
