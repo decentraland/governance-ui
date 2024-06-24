37a55d918ad6a203dfc361893d63027d897367fa
1emu <github@lemu.sh>
2024-06-24 16:59:27 -0300

    feat: add feature release date limit, remove logs

--- src/hooks/useShowProjectUpdatesCta.ts
+++ src/hooks/useShowProjectUpdatesCta.ts
@@ -4,6 +4,8 @@ import { Project } from '../types/proposals.ts'
 import useIsProjectEditor from './useIsProjectEditor.ts'
 import useProjectUpdates from './useProjectUpdates.ts'
 
+const UPDATED_CTA_FEATURE_RELEASE_DATE = new Date('2023-06-26')
+
 function getStoredDate(key: string): Date | null {
   const dateString = localStorage.getItem(key)
   if (dateString && dateString.length > 0) {
@@ -18,18 +20,16 @@ function getStoredDate(key: string): Date | null {
 export default function useShowProjectUpdatesCta(project?: Project | null) {
   const { isEditor } = useIsProjectEditor(project)
   const { hasPendingMandatoryUpdate, latestPublishedUpdate } = useProjectUpdates(project?.id)
-
+  const now = new Date()
   const savedDate = getStoredDate(`${PROJECT_UPDATES_LATEST_CHECK}-${project?.id}`)
-  console.log('savedDate', savedDate)
   const latestCheckToUpdatesTab = savedDate ? new Date(savedDate) : null
   const editorHasPendingMandatoryUpdates = isEditor && hasPendingMandatoryUpdate
   const newUpdateForNonEditors =
     !isEditor &&
     latestPublishedUpdate &&
     ((latestCheckToUpdatesTab && latestPublishedUpdate.updated_at > latestCheckToUpdatesTab) ||
-      latestCheckToUpdatesTab === null)
+      latestCheckToUpdatesTab === null) &&
+    now > UPDATED_CTA_FEATURE_RELEASE_DATE
 
-  console.log('editorHasPendingMandatoryUpdates', editorHasPendingMandatoryUpdates)
-  console.log('newUpdateForNonEditors', newUpdateForNonEditors)
   return { showUpdatesCta: editorHasPendingMandatoryUpdates || newUpdateForNonEditors }
 }
