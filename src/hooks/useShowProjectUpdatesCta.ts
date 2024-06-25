a3159fa9c01d3d4237e0bc547e90fc4e94312499
1emu <github@lemu.sh>
2024-06-25 12:26:48 -0300

    refactor: use time js for date comparisons

--- src/hooks/useShowProjectUpdatesCta.ts
+++ src/hooks/useShowProjectUpdatesCta.ts
@@ -1,17 +1,18 @@
 import { PROJECT_UPDATES_LATEST_CHECK } from '../localStorageKeys.ts'
 import { Project } from '../types/proposals.ts'
+import Time from '../utils/date/Time.ts'
 
 import useIsProjectEditor from './useIsProjectEditor.ts'
 import useProjectUpdates from './useProjectUpdates.ts'
 
-const UPDATED_CTA_FEATURE_RELEASE_DATE = new Date('2023-06-26')
+const UPDATED_CTA_FEATURE_RELEASE_DATE = Time('2024-06-25T00:00:00Z')
 
-function getStoredDate(key: string): Date | null {
+function getStoredTime(key: string): Time.Dayjs | null {
   const dateString = localStorage.getItem(key)
   if (dateString && dateString.length > 0) {
-    const date = new Date(dateString)
-    if (!isNaN(date.getTime())) {
-      return date
+    const time = Time(dateString)
+    if (time.isValid()) {
+      return time
     }
   }
   return null
@@ -20,15 +21,13 @@ function getStoredDate(key: string): Date | null {
 export default function useShowProjectUpdatesCta(project?: Project | null) {
   const { isEditor } = useIsProjectEditor(project)
   const { hasPendingMandatoryUpdate, latestPublishedUpdate } = useProjectUpdates(project?.id)
-  const now = new Date()
-  const latestCheckToUpdatesTab = getStoredDate(`${PROJECT_UPDATES_LATEST_CHECK}-${project?.id}`)
+  const latestCheckToUpdatesTab = getStoredTime(`${PROJECT_UPDATES_LATEST_CHECK}-${project?.id}`)
   const editorHasPendingMandatoryUpdates = isEditor && hasPendingMandatoryUpdate
   const newUpdateForNonEditors =
     !isEditor &&
     !!latestPublishedUpdate &&
-    ((latestCheckToUpdatesTab && latestPublishedUpdate.updated_at > latestCheckToUpdatesTab) ||
-      latestCheckToUpdatesTab === null) &&
-    now > UPDATED_CTA_FEATURE_RELEASE_DATE
+    ((!!latestCheckToUpdatesTab && latestCheckToUpdatesTab.isBefore(latestPublishedUpdate.updated_at)) ||
+      (latestCheckToUpdatesTab === null && UPDATED_CTA_FEATURE_RELEASE_DATE.isBefore(latestPublishedUpdate.updated_at)))
 
   return { showUpdatesCta: editorHasPendingMandatoryUpdates || newUpdateForNonEditors }
 }
